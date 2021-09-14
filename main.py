cb96e0d06e6b010680c4910145366de73015e0eb
Alexandre Petitjean <alpetitjean@gmail.com>
2021-09-14 09:58:54 +0200

    Rename app in help

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from npmmon.config import Config
 from npmmon.runner import LogsTailRunner, ReadAllRunner
 
 # Parse args
-parser = argparse.ArgumentParser(description='NPM IP monitor')
+parser = argparse.ArgumentParser(description='NginxProxyManager IP monitor')
 parser.add_argument('-c', '--config', action='store', required=True)
 parser.add_argument('-w', '--watch', action='store_true')
 args = parser.parse_args()
@@ -13,6 +13,7 @@ args = parser.parse_args()
 # Load config
 config = Config()
 config.load_yaml(args.config)
+config.load_from_env()
 
 # Run
 runner = LogsTailRunner(config) if args.watch else ReadAllRunner(config)
